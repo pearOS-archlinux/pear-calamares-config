45390ebec8df532b5b6459a3f31536344849edbb
Alexandru Balan <alxb421@gmail.com>
2026-03-03 05:10:33 +0200

    test

--- etc/calamares/launch.sh
+++ etc/calamares/launch.sh
@@ -5,7 +5,28 @@ KERNEL=$(uname -r)
 
 if [[ -d "/run/archiso/copytoram" ]]; then
 	sudo sed -i -e 's|/run/archiso/bootmnt/arch/x86_64/airootfs.sfs|/run/archiso/copytoram/airootfs.sfs|g' "$DIR"/modules/unpackfs.conf
-	sudo sed -i -e "s|/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-linux|/usr/lib/modules/$KERNEL/vmlinuz|g" "$DIR"/modules/unpackfs.conf
+fi
+
+# Alege robust sursa kernel-ului (evită fișiere 0B / căi inexistente)
+VMLINUX_CANDIDATES=(
+	"/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-linux"
+	"/run/archiso/bootmnt/boot/vmlinuz-linux"
+	"/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz"
+	"/usr/lib/modules/$KERNEL/vmlinuz"
+	"/usr/lib/modules/$KERNEL/vmlinuz-linux"
+)
+
+VMLINUX_SRC=""
+for p in "${VMLINUX_CANDIDATES[@]}"; do
+	if [[ -s "$p" ]]; then
+		VMLINUX_SRC="$p"
+		break
+	fi
+done
+
+if [[ -n "$VMLINUX_SRC" ]]; then
+	# Înlocuiește doar intrarea de kernel din unpackfs.conf
+	sudo sed -i -E "s|source: \"[^\"]*/vmlinuz[^\"]*\"|source: \"$VMLINUX_SRC\"|g" "$DIR"/modules/unpackfs.conf
 fi
 
 # Alegerea Offline / Netinstall se face în Calamares (pas „Tip instalare” la începutul exec)
